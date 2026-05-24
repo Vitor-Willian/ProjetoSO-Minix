6b81f0fca8e8119de23d2b1380f53e94e4a6f172
Vitor-Willian <vitor.peneluppi@unifesp.br>
2026-05-24 16:21:21 -0300

    Eliminate priority adjustment in scheduling
    Removed priority decrement logic from scheduling.

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -96,9 +96,12 @@ int do_noquantum(message *m_ptr)
 	}
 
 	rmp = &schedproc[proc_nr_n];
-	if (rmp->priority < MIN_USER_Q) {
-		rmp->priority += 1; /* lower priority */
-	}
+
+	//ALTERAÇÃO: remoção do decremento de prioridade com o tempo
+	
+	// if (rmp->priority < MIN_USER_Q) {
+	// 	rmp->priority += 1; /* lower priority */
+	// }
 
 	if ((rv = schedule_process_local(rmp)) != OK) {
 		return rv;
@@ -352,17 +355,8 @@ void init_scheduling(void)
  */
 void balance_queues(void)
 {
-	struct schedproc *rmp;
-	int r, proc_nr;
-
-	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
-		if (rmp->flags & IN_USE) {
-			if (rmp->priority > rmp->max_priority) {
-				rmp->priority -= 1; /* increase priority */
-				schedule_process_local(rmp);
-			}
-		}
-	}
+	// ALTERAÇÃO: remoção do rebalanceamento para manter prioridade fixa
+	int r;
 
 	if ((r = sys_setalarm(balance_timeout, 0)) != OK)
 		panic("sys_setalarm failed: %d", r);
