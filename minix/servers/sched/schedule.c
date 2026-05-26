e75cd7c5909071b9b82a28100079055740ba373a
ErikReis15 <erik.reis11@unifesp.br>
2026-05-26 09:20:53 -0300

    Atualizando scheduler.c para FCFS
    Altered scheduling logic to implement FCFS without priority handling.

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -38,7 +38,8 @@ static int schedule_process(struct schedproc * rmp, unsigned flags);
 
 #define cpu_is_available(c)	(cpu_proc[c] >= 0)
 
-#define DEFAULT_USER_TIME_SLICE 200
+/* Aumentando o tempo para o valor máximo permitido */
+#define DEFAULT_USER_TIME_SLICE 0x7FFFFFFF
 
 /* processes created by RS are sysytem processes */
 #define is_system_proc(p)	((p)->parent == RS_PROC_NR)
@@ -86,23 +87,9 @@ static void pick_cpu(struct schedproc * proc)
 
 int do_noquantum(message *m_ptr)
 {
-	register struct schedproc *rmp;
-	int rv, proc_nr_n;
-
-	if (sched_isokendpt(m_ptr->m_source, &proc_nr_n) != OK) {
-		printf("SCHED: WARNING: got an invalid endpoint in OOQ msg %u.\n",
-		m_ptr->m_source);
-		return EBADEPT;
-	}
-
-	rmp = &schedproc[proc_nr_n];
-	if (rmp->priority < MIN_USER_Q) {
-		rmp->priority += 1; /* lower priority */
-	}
-
-	if ((rv = schedule_process_local(rmp)) != OK) {
-		return rv;
-	}
+	/*Funcao trata quando o quantum do processo acaba, diminuindo prioridade
+	* e reescalonando novamente, porem FCFS nao utiliza usa prioridade
+	*/
 	return OK;
 }
 
@@ -171,7 +158,12 @@ int do_start_scheduling(message *m_ptr)
 	if (rmp->endpoint == rmp->parent) {
 		/* We have a special case here for init, which is the first
 		   process scheduled, and the parent of itself. */
-		rmp->priority   = USER_Q;
+		
+		/* Altereando para prioridade 0, para utilizar apenas uma fila 
+		*  e definindo o tamanho do quantum como valor máximo, para utilizar o
+		* Round-Robin como FCFS
+		*/
+		rmp->priority   = 0;
 		rmp->time_slice = DEFAULT_USER_TIME_SLICE;
 
 		/*
@@ -192,8 +184,13 @@ int do_start_scheduling(message *m_ptr)
 		/* We have a special case here for system processes, for which
 		 * quanum and priority are set explicitly rather than inherited 
 		 * from the parent */
-		rmp->priority   = rmp->max_priority;
-		rmp->time_slice = m_ptr->m_lsys_sched_scheduling_start.quantum;
+		
+		/* Altereando para prioridade 0, para utilizar apenas uma fila 
+		*  e definindo o tamanho do quantum como valor máximo, para utilizar o
+		* Round-Robin como FCFS
+		*/
+		rmp->priority   = 0;
+		rmp->time_slice = DEFAULT_USER_TIME_SLICE;
 		break;
 		
 	case SCHEDULING_INHERIT:
@@ -204,8 +201,13 @@ int do_start_scheduling(message *m_ptr)
 				&parent_nr_n)) != OK)
 			return rv;
 
-		rmp->priority = schedproc[parent_nr_n].priority;
-		rmp->time_slice = schedproc[parent_nr_n].time_slice;
+		/* Altereando para prioridade 0, para utilizar apenas uma fila 
+		*  e definindo o tamanho do quantum como valor máximo, para utilizar
+		*  como FCFS
+		*/
+		rmp->priority   = 0;
+		rmp->time_slice = DEFAULT_USER_TIME_SLICE;
+		
 		break;
 		
 	default: 
@@ -253,42 +255,8 @@ int do_start_scheduling(message *m_ptr)
  *===========================================================================*/
 int do_nice(message *m_ptr)
 {
-	struct schedproc *rmp;
-	int rv;
-	int proc_nr_n;
-	unsigned new_q, old_q, old_max_q;
-
-	/* check who can send you requests */
-	if (!accept_message(m_ptr))
-		return EPERM;
-
-	if (sched_isokendpt(m_ptr->m_pm_sched_scheduling_set_nice.endpoint, &proc_nr_n) != OK) {
-		printf("SCHED: WARNING: got an invalid endpoint in OoQ msg "
-		"%d\n", m_ptr->m_pm_sched_scheduling_set_nice.endpoint);
-		return EBADEPT;
-	}
-
-	rmp = &schedproc[proc_nr_n];
-	new_q = m_ptr->m_pm_sched_scheduling_set_nice.maxprio;
-	if (new_q >= NR_SCHED_QUEUES) {
-		return EINVAL;
-	}
-
-	/* Store old values, in case we need to roll back the changes */
-	old_q     = rmp->priority;
-	old_max_q = rmp->max_priority;
-
-	/* Update the proc entry and reschedule the process */
-	rmp->max_priority = rmp->priority = new_q;
-
-	if ((rv = schedule_process_local(rmp)) != OK) {
-		/* Something went wrong when rescheduling the process, roll
-		 * back the changes to proc struct */
-		rmp->priority     = old_q;
-		rmp->max_priority = old_max_q;
-	}
-
-	return rv;
+	/* FCFS nao tem prioridade, removendo a logica */
+	return OK;
 }
 
 /*===========================================================================*
@@ -301,22 +269,19 @@ static int schedule_process(struct schedproc * rmp, unsigned flags)
 
 	pick_cpu(rmp);
 
-	if (flags & SCHEDULE_CHANGE_PRIO)
-		new_prio = rmp->priority;
-	else
-		new_prio = -1;
-
-	if (flags & SCHEDULE_CHANGE_QUANTUM)
-		new_quantum = rmp->time_slice;
-	else
-		new_quantum = -1;
+	/* FCFS nao tem prioridade */
+    new_prio = 0;
+    
+	/* Setando quantum como valor máximo para simular FCFS */
+	new_quantum = DEFAULT_USER_TIME_SLICE;
 
 	if (flags & SCHEDULE_CHANGE_CPU)
 		new_cpu = rmp->cpu;
 	else
 		new_cpu = -1;
 
-	niced = (rmp->max_priority > USER_Q);
+	/* FCFS nao tem prioridade, entao processos de usuario tambem nao*/
+	niced = 0;
 
 	if ((err = sys_schedule(rmp->endpoint, new_prio,
 		new_quantum, new_cpu, niced)) != OK) {
@@ -355,15 +320,8 @@ void balance_queues(void)
 	struct schedproc *rmp;
 	int r, proc_nr;
 
-	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
-		if (rmp->flags & IN_USE) {
-			if (rmp->priority > rmp->max_priority) {
-				rmp->priority -= 1; /* increase priority */
-				schedule_process_local(rmp);
-			}
-		}
-	}
-
+  /* FCFS nao utiliza balanceamento de filas, removendo a logica  */
+	
 	if ((r = sys_setalarm(balance_timeout, 0)) != OK)
 		panic("sys_setalarm failed: %d", r);
 }
